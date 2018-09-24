0b49d851a8996ba1afefeb20598d21029f13e292
hachedeeme <hector.maidana@trinom.io>
2018-09-24 18:08:25 -0300

    Actualizada la data del ninemanga provider.

--- main.py
+++ main.py
@@ -34,26 +34,31 @@ if __name__== "__main__":
       'op' : Kissmanga('one_piece_color')
     },
     "ninemanga": {
-      'ookii': Ninemanga('ookii_onnanoko_wa_daisuki'),
-      'mh'   : Ninemanga('mouhitsu_hallucination'),
-      'ps'   : Ninemanga('prison_school'),
-      'hikyu': Ninemanga('haikyuu'),
-      'nnt'  : Ninemanga('nanatsu_no_taizai'),
-      'bnha' : Ninemanga('boku_no_hero_academia'),
-      'vbnha': Ninemanga('vigilante_boku_no_hero_academia'),
-      'bbt'  : Ninemanga('boin_boin_teacher'),
-      'hni'  : Ninemanga('hajime_no_ippo')
+      'op'    : Ninemanga('one_piece'),
+      'beelz' : Ninemanga('beelzebub'),
+      'bers'  : Ninemanga('berserk'),
+      'ct2002': Ninemanga('captain_tsubasa_road_to_2002'),
+      'ookii' : Ninemanga('ookii_onnanoko_wa_daisuki'),
+      'mh'    : Ninemanga('mouhitsu_hallucination'),
+      'ps'    : Ninemanga('prison_school'),
+      'hikyu' : Ninemanga('haikyuu'),
+      'nnt'   : Ninemanga('nanatsu_no_taizai'),
+      'hxh'   : Ninemanga('hunter_x_hunter'),
+      'bnha'  : Ninemanga('boku_no_hero_academia'),
+      'vbnha' : Ninemanga('vigilante_boku_no_hero_academia'),
+      'bbt'   : Ninemanga('boin_boin_teacher'),
+      'hni'   : Ninemanga('hajime_no_ippo')
     }
   }
   
   provider = providers['jokerfansub']['op']
-  provider = providers['ninemanga']['bbt']
+  provider = providers['ninemanga']['beelz']
   # provider.set_chapters([978,979])
-  provider.set_chapters(range(1,46))
+  # provider.set_chapters(range(1,46))
 
   md = MangaDownloader(provider)
   # md.enumerate_exceptions()
   # md.upload()
-  md.download_mangas()
+  # md.download_mangas()
   # md.upload_and_donwload_last()
-  # md.download_last(4)
+  # md.download_last(3)
