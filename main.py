b130d77ba30f6ccccd2116519cc8e57c9e39680d
hachedeeme <hector.maidana@trinom.io>
2018-10-03 18:21:55 -0300

    Agregada nueva data de ninemanga.

--- main.py
+++ main.py
@@ -5,19 +5,43 @@ from providers.kissmanga   import Kissmanga
 from providers.ninemanga   import Ninemanga
 
 if __name__== "__main__":
-  # +=======================+========================+=============+=========+
-  # |          NAME         |        MANGA ID        | JOKERFANSUB | INMANGA |
-  # +=======================+========================+=============+=========+
-  # | One Piece             | one_piece              |      O      |    O    |
-  # +-----------------------+------------------------+-------------+---------+
-  # | Nanatsu No Taizai     | nanatsu_no_taizai      |             |    O    |
-  # +-----------------------+------------------------+-------------+---------+
-  # | Noku No Hero Academia | boku_no_hero_academia  |             |    O    |
-  # +-----------------------+------------------------+-------------+---------+
-  # | Hajime no Ippo        | hajime_no_ippo         |             |    O    |
-  # +-----------------------+------------------------+-------------+---------+
-  # | Dragon Ball Super     | dragon_ball_super      |             |    O    |
-  # +-----------------------+------------------------+-------------+---------+
+  # +==================================+=================================+=============+=========+===========+
+  # |                     NAME         |                 MANGA ID        | JOKERFANSUB | INMANGA | NINEMANGA |
+  # +==================================+=================================+=============+=========+===========+
+  # | Beelzebub                        | beelzebub                       |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Berserk                          | berserk                         |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Boin Boin Teacher                | boin_boin_teacher               |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Noku No Hero Academia            | boku_no_hero_academia           |             |    O    |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Captain Tsubasa Road to 2002     | captain_tsubasa_road_to_2002    |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Dragon Ball Super                | dragon_ball_super               |      O      |    O    |           |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Haikyuu                          | haikyuu                         |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Hajime no Ippo                   | hajime_no_ippo                  |             |    O    |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Hinomaru Zumou                   | hinomaru_zumou                  |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Hunter X Hunter                  | hunter_x_hunter                 |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Kimi Wa 008                      | kimi_wa_008                     |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Mouhitsu Hallucination           | mouhitsu_hallucination          |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Nanatsu no Taizai                | nanatsu_no_taizai               |             |    O    |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | One Piece                        | one_piece                       |      O      |    O    |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Ookii Onnanoko Wa Daisuki        | ookii_onnanoko_wa_daisuki       |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Prison School                    | prison_school                   |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
+  # | Vigilante: Noku No Hero Academia | vigilante_boku_no_hero_academia |             |         |     O     |
+  # +----------------------------------+---------------------------------+-------------+---------+-----------+
   providers = {
     "jokerfansub": {
       'op'  : JokerFansub('one_piece'),
@@ -34,20 +58,22 @@ if __name__== "__main__":
       'op' : Kissmanga('one_piece_color')
     },
     "ninemanga": {
-      'op'    : Ninemanga('one_piece'),
-      'beelz' : Ninemanga('beelzebub'),
-      'bers'  : Ninemanga('berserk'),
-      'ct2002': Ninemanga('captain_tsubasa_road_to_2002'),
-      'ookii' : Ninemanga('ookii_onnanoko_wa_daisuki'),
-      'mh'    : Ninemanga('mouhitsu_hallucination'),
-      'ps'    : Ninemanga('prison_school'),
-      'hikyu' : Ninemanga('haikyuu'),
-      'nnt'   : Ninemanga('nanatsu_no_taizai'),
-      'hxh'   : Ninemanga('hunter_x_hunter'),
-      'bnha'  : Ninemanga('boku_no_hero_academia'),
-      'vbnha' : Ninemanga('vigilante_boku_no_hero_academia'),
-      'bbt'   : Ninemanga('boin_boin_teacher'),
-      'hni'   : Ninemanga('hajime_no_ippo')
+      'beelz' : Ninemanga('beelzebub'), 
+      'bers'  : Ninemanga('berserk'), 
+      'bbt'   : Ninemanga('boin_boin_teacher'), 
+      'bnha'  : Ninemanga('boku_no_hero_academia'), 
+      'ct2002': Ninemanga('captain_tsubasa_road_to_2002'), 
+      'hikyu' : Ninemanga('haikyuu'), 
+      'hni'   : Ninemanga('hajime_no_ippo'), 
+      'hz'    : Ninemanga('hinomaru_zumou'), 
+      'hxh'   : Ninemanga('hunter_x_hunter'), 
+      'kw008' : Ninemanga('kimi_wa_008'), 
+      'mh'    : Ninemanga('mouhitsu_hallucination'), 
+      'nnt'   : Ninemanga('nanatsu_no_taizai'), 
+      'op'    : Ninemanga('one_piece'), 
+      'ookii' : Ninemanga('ookii_onnanoko_wa_daisuki'), 
+      'ps'    : Ninemanga('prison_school'), 
+      'vbnha' : Ninemanga('vigilante_boku_no_hero_academia')
     }
   }
   
